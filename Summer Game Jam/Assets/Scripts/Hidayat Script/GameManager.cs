c263b69b0cbb8ceec3dd6246f0e9952cae0d3f88
Hidayat <hidhandsome143@gmail.com>
2017-07-29 00:53:40 +0300

    commit

---
+++ Summer Game Jam/Assets/Scripts/Hidayat Script/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public OrderManager order_manager;
+    public DrunkPressureManager dp_manager;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string status = dp_manager.UpdateValues();
+    }
+
+    /// <summary>
+    /// Instantiates a customer to animate and create the order
+    /// </summary>
+    void Create_NewCustomer()
+    {
+        GameObject customer = null;//Instantiate();
+        order_manager.AddCustomer(customer);
+        Make_Order(customer);
+    }
+
+    void Make_Order(GameObject customer)
+    {
+        //customer.GetComponent<OrderManager>().Order();
+    }
+}
--- Summer Game Jam/Assets/Scripts/Hidayat Script/OrderManager.cs
+++ Summer Game Jam/Assets/Scripts/Hidayat Script/OrderManager.cs
@@ -1,16 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class OrderManager : MonoBehaviour {
 
+    Queue<GameObject> customers;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        customers = new Queue<GameObject>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// When a new customer enters the line to order Ice Cream
+    /// </summary>
+    /// <param name="customer"></param>
+    public void AddCustomer(GameObject customer)
+    {
+        customers.Enqueue(customer);
+    }
+
+    /// <summary>
+    /// Removing the first customer in the queue
+    /// </summary>
+    public void RemoveCustomer()
+    {
+        customers.Dequeue();
+    }
+    
+    /// <summary>
+    /// Returns if the order created by player is the same as order from customers
+    /// </summary>
+    /// <returns></returns>
+    public bool CheckOrder()
+    {
+        //foreach (GameObject customer in customers)
+        //{
+        //    //  if (customer.order == order)
+        //    //      customer.GetComponent<Order>();
+        //}
+        return false;
+    }
 }
