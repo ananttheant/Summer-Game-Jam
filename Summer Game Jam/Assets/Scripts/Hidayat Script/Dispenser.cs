002b71e7c597f8a9cc6e1fe7824db23ce5727014
Anant Sharma <ananttheant4444@gmail.com>
2017-07-30 04:44:34 +0300

    Refactoring

--- Summer Game Jam/Assets/Scripts/Hidayat Script/Dispenser.cs
+++ Summer Game Jam/Assets/Scripts/Hidayat Script/Dispenser.cs
@@ -7,20 +7,20 @@ public class Dispenser : MonoBehaviour
     public GameObject[] output; // this is the game object that should be present in the scene , cuz it will get duplicated.
 
     
-    KeyCode lastPressed;
-    float timeToPress = 1;
-    float startTime = 0;
+    private KeyCode _lastPressed;
+    private const float TimeToPress = 1;
+    private float _startTime = 0;
     // Update is called once per frame
 	void Update ()
     {
-        if (Time.time > startTime + timeToPress)
+        if (Time.time > _startTime + TimeToPress)
         {
             for (int i = 0; i < keyPressed.Length; i++)
             {
                 if (Input.GetKeyDown(keyPressed[i]))
                 {
-                    startTime = Time.time;
-                    lastPressed = keyPressed[i];
+                    _startTime = Time.time;
+                    _lastPressed = keyPressed[i];
                     GetComponent<Animator>().SetBool("Dispense", true);
                 }
             }
@@ -31,7 +31,7 @@ public class Dispenser : MonoBehaviour
     {
         for (int i = 0; i < keyPressed.Length; i++)
         {
-            if (keyPressed[i] == lastPressed)
+            if (keyPressed[i] == _lastPressed)
             {
                 Vector3 positionToSpawn = transform.GetChild(0).position;
                 GameObject ingredients = Instantiate(output[i], positionToSpawn, Quaternion.identity, transform.root.GetChild(0));
